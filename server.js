9147a8ca67212790cf515dae98d7aefeb50afcb0
ronjoshi-unc <ronjoshi@ad.unc.edu>
2022-11-20 18:19:36 -0500

    attempt 3

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ import minimist from 'minimist';
 import { roll } from './lib/roll.js';
 
 const app = express();
+
+app.use(express.urlencoded({extended: true}));
 const args = minimist(process.argv.slice(2));
 
 const port = args.port || 5000;
